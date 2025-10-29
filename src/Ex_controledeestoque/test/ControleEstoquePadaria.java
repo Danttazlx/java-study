07f99888265c56d921559c6ec9dd240efc0e3e85
daniel <danttaz11@gmail.com>
2025-10-29 20:51:37 -0300

    exercicio 01

---
+++ src/Ex_controledeestoque/test/ControleEstoquePadaria.java
@@ -0,0 +1,19 @@
+package Ex_controledeestoque.test;
+
+public class ControleEstoquePadaria {
+
+    public static void main(String[] args) {
+
+        int quantidadeFarinha = 10;
+
+        int quantidadeDeAcucar = 5;
+
+        System.out.println("quantidade de farinha é: " + quantidadeFarinha);
+        System.out.println("quantidade de acucar é: " + quantidadeDeAcucar);
+
+
+    }
+
+
+
+}

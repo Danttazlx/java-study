ce87769c9c667318863581fa13764bc61cde8d04
daniel <danttaz11@gmail.com>
2025-10-29 21:27:53 -0300

    operadores de atribuicao

---
+++ src/Ex_controledeestoque/operadoresDeAtribuicao/main.java
@@ -0,0 +1,30 @@
+package Ex_controledeestoque.operadoresDeAtribuicao;
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+//        int a = 10;
+//        var d = a -= 2;
+//
+//        System.out.println(d);
+
+
+
+//        int a = 10;
+//        var d = a *= 2;                          ele esta fazendo 10 mutiplicado por 2
+//
+//        System.out.println(d);
+
+
+        int a = 10;
+        var d = a /= 2;                     // divisao
+
+        System.out.println(d);
+
+
+
+    }
+}
+

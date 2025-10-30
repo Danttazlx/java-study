96c3ee5306025fa0b62132004ca9fbd5df80e22f
daniel <danttaz11@gmail.com>
2025-10-29 22:20:30 -0300

    instanceOF

---
+++ src/estudo_java/instanceof_/CompararTipos.java
@@ -0,0 +1,30 @@
+package estudo_java.instanceof_;
+
+public class CompararTipos {
+    public static void main(String[] args) {
+//
+//        Object object = " ";
+//
+//        if(object instanceof String){
+//            System.out.println("true");
+//
+//        }else
+//            System.out.println("false");
+
+
+
+        Object object = " ";
+
+        if(object instanceof Integer){
+            System.out.println("true");                    //ele compara
+
+        }else
+            System.out.println("false");
+
+
+
+
+    }
+
+
+}

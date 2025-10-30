120af2a6fdae45122ccff29fa4514b567c23ad7f
daniel <danttaz11@gmail.com>
2025-10-29 21:47:37 -0300

    calculadora com scanner

---
+++ src/exercicio/Calculadora.java
@@ -0,0 +1,26 @@
+package exercicio;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("digite um numero: ");
+        int a = scanner.nextInt();
+
+
+        System.out.println("digite outro numero: ");
+        int b = scanner.nextInt();
+
+        var soma = a + b;
+
+        System.out.println("A soma entre os dois numeros é " + soma);
+
+
+    }
+
+
+}

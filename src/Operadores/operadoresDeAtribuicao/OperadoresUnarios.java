9d7b820b1ba199838de82b0f1053fd9e59d09eba
daniel <danttaz11@gmail.com>
2025-10-29 21:39:50 -0300

    operadores de Unarios

---
+++ src/Operadores/operadoresDeAtribuicao/OperadoresUnarios.java
@@ -0,0 +1,21 @@
+package Operadores.operadoresDeAtribuicao;
+
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        a++;                      incremento   adiciona 1
+//              var d = a = a +1;
+//
+//        System.out.println(a);
+
+
+        int a = 10;
+        a--;
+        //  var d = a= a - 1;                 decremento diminui 1
+
+        System.out.println(a);
+
+
+    }
+}

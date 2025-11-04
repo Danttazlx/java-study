77b83b990ce11a48328416dbbc8036c1eb0e2d1d
daniel <danttaz11@gmail.com>
2025-11-04 20:49:07 -0300

    laco fori

---
+++ src/estudo_java/lacoderepeticao/For.java
@@ -0,0 +1,12 @@
+package estudo_java.lacoderepeticao;
+
+public class For {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <5; i++) {
+
+            System.out.println(i);
+        }
+    }
+}

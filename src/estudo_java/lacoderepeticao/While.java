3306e0aa228ac8b43d1ecbb39e307b3fa6a75300
daniel <danttaz11@gmail.com>
2025-11-04 20:53:33 -0300

    laco while

---
+++ src/estudo_java/lacoderepeticao/While.java
@@ -0,0 +1,17 @@
+package estudo_java.lacoderepeticao;
+
+public class While {
+    public static void main(String[] args) {
+
+        int numero = 0;
+
+        while (numero < 10){  // ele retorna um valor boleano
+
+            System.out.println(numero);
+
+            numero++;
+        }
+
+
+    }
+}

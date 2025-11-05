483b7254c97067e14235c08a7f5e4b4e20c6d795
daniel <danttaz11@gmail.com>
2025-11-04 21:02:44 -0300

    laco Do-while

---
+++ src/estudo_java/lacoderepeticao/DoWhile.java
@@ -0,0 +1,19 @@
+package estudo_java.lacoderepeticao;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        int a = 0;
+
+        // faca isso
+        // ele garante que bloco do rode ao menos uma vez
+
+        do {
+
+            System.out.println(a);
+            a++;
+
+        } while (a < 5);
+
+    }
+}

7c97ff613732733068160a424faeabeea93bf59b
daniel <danttaz11@gmail.com>
2025-11-04 21:18:57 -0300

    exercicio

---
+++ src/estudo_java/lacoderepeticao/Exercicio.java
@@ -0,0 +1,15 @@
+package estudo_java.lacoderepeticao;
+
+public class Exercicio {
+    public static void main(String[] args) {
+
+        int soma = 0;
+
+        for (int i = 1; i <= 10; i++) {
+
+           soma += i;  // aqui a variavel valor esta recebndo +1
+
+        }
+        System.out.println(soma);
+    }
+}

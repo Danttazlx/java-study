0d058e08b5ce18495d7875f1647848d678fcb32e
daniel <danttaz11@gmail.com>
2025-11-14 22:50:50 -0300

    metodo static

---
+++ src/estudo_java/membrostatic/Calculdora.java
@@ -0,0 +1,14 @@
+package estudo_java.membrostatic;
+
+public class Calculdora {
+
+        public static int somar(int a, int b){
+         var soma = a + b;
+        System.out.println(soma);
+        return soma;
+
+    }
+
+
+
+}
---
+++ src/estudo_java/membrostatic/Test.java
@@ -0,0 +1,10 @@
+package estudo_java.membrostatic;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Calculdora.somar(10,10);   // voce acessa pelo nome da class.
+
+
+    }
+}

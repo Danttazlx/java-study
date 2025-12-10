11fb563262e1d38f412b44cd644b438dbe67aa04
daniel <danttaz11@gmail.com>
2025-12-09 21:54:24 -0300

    estudo Set

---
+++ src/estudo_java/collection/SetEstudo.java
@@ -0,0 +1,19 @@
+package estudo_java.collection;
+
+import java.util.*;
+
+public class SetEstudo {
+    public static void main(String[] args) {
+
+        Set<String> nomes = new HashSet<>();
+
+        nomes.add("");
+        nomes.add("");
+        System.out.println("contem esse elemento: " + nomes.contains("Dantas"));
+
+
+
+
+
+    }
+}

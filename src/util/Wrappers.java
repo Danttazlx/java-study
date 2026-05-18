d718d1330264197b261b403aed71715a4ff384fe
Daniel <Danttaz11@gmail.com>
2026-05-18 20:06:11 -0300

    estudp class wrappers

---
+++ src/util/Wrappers.java
@@ -0,0 +1,26 @@
+package util;
+
+public class Wrappers {
+    public static void main(String[] args) {
+
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 1;
+        float floatP = 1;
+        double doubleP = 1;
+        boolean booleanP = true;
+        char charP = 'm';
+
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 1L;
+        Float floatW = 1F;
+        Double doubleW = 1D;
+        Boolean booleanW = true;
+        Character charW = 'm';
+
+    }
+}
